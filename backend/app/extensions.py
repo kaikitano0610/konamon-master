444ee072150208d34922f24b42125a8612491695
ippo_hiroi <ippohiroi@gmail.com>
2025-07-28 00:26:07 +0900

    feat: dockerでRDBを管理できるように

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,6 +1,13 @@
 # backend/app/extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+
+MYSQL_USER = os.environ.get("MYSQL_USER")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+MYSQL_PORT = os.environ.get("MYSQL_PORT", "3306")
+MYSQL_NAME = os.environ.get("MYSQL_DB")
 
 db = SQLAlchemy()
 migrate = Migrate()
